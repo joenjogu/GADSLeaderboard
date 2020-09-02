b1f463c65e2c0e41892efae4bf78d883afc89ad5
Joe Njogu <joenjogu97@gmail.com>
2020-09-03 01:57:48 +0300

    Added RecyclerViews to LearningAdapter and SkillAdapter

--- app/src/main/java/com/joenjogu/gadsleaderboard/LearningAdapter.java
+++ app/src/main/java/com/joenjogu/gadsleaderboard/LearningAdapter.java
@@ -1,19 +1,36 @@
 package com.joenjogu.gadsleaderboard;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class LearningAdapter extends RecyclerView.Adapter {
+public class LearningAdapter extends RecyclerView.Adapter<LearningAdapter.LearningViewHolder> {
+
+    private LayoutInflater mInflater;
+    public LearningAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+    }
+
+    static class LearningViewHolder extends RecyclerView.ViewHolder{
+
+        public LearningViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public LearningViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.hours_recyclerview_item, parent, false);
+        return new LearningViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull LearningViewHolder holder, int position) {
 
     }
 
--- app/src/main/java/com/joenjogu/gadsleaderboard/SkillAdapter.java
+++ app/src/main/java/com/joenjogu/gadsleaderboard/SkillAdapter.java
@@ -1,19 +1,37 @@
 package com.joenjogu.gadsleaderboard;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SkillAdapter extends RecyclerView.Adapter {
+public class SkillAdapter extends RecyclerView.Adapter<SkillAdapter.SkillViewHolder> {
+
+    private LayoutInflater mInflater;
+
+    public SkillAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+    }
+
+    static class SkillViewHolder extends RecyclerView.ViewHolder{
+
+        public SkillViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public SkillViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.iq_recyclerview_item, parent, false);
+        return new SkillViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull SkillViewHolder holder, int position) {
 
     }
 
