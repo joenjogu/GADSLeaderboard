2e3876b650dd056ff0bc41066a6fd4e816b05e3e
Joe Njogu <joenjogu97@gmail.com>
2020-09-09 22:14:48 +0300

    Added a splash screen

---
+++ app/src/main/java/com/joenjogu/gadsleaderboard/SplashScreenActivity.java
@@ -0,0 +1,18 @@
+package com.joenjogu.gadsleaderboard;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
