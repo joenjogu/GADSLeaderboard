c8a2df553b7a97ad6aba33d3e8deb9a4add4e6d3
Joe Njogu <joenjogu97@gmail.com>
2020-09-11 12:26:19 +0300

    Refactored NetworkRepository.java

--- app/src/main/java/com/joenjogu/gadsleaderboard/NetworkRepository.java
+++ app/src/main/java/com/joenjogu/gadsleaderboard/NetworkRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import retrofit2.Call;
 
+@SuppressWarnings("FieldCanBeLocal")
 public class NetworkRepository {
 
     private LearningHoursService hoursService;
@@ -12,19 +13,16 @@ public class NetworkRepository {
 
     public Call<List<LearningHours>> getHoursCall() {
         hoursService = ServiceBuilder.buildService(LearningHoursService.class);
-        Call<List<LearningHours>> hoursCall = hoursService.getLearningHoursLeaderboard();
-        return hoursCall;
+        return hoursService.getLearningHoursLeaderboard();
     }
 
     public Call<List<SkillIQ>> getSkillCall() {
         skillService = ServiceBuilder.buildService(SkillService.class);
-        Call<List<SkillIQ>> skillCall = skillService.getSkillIqLeaderboard();
-        return skillCall;
+        return skillService.getSkillIqLeaderboard();
     }
 
     public Call<Void> postUserDetails(String firstName, String lastName, String email, String link) {
         submissionService = ServiceBuilder.buildService(SubmissionService.class);
-        Call<Void> submitCall = submissionService.submitUserDetails(firstName, lastName, email, link);
-        return submitCall;
+        return submissionService.submitUserDetails(firstName, lastName, email, link);
     }
 }
