252e13f5c6ff8c0a4353765859581384bd579ac1
Joe Njogu <joenjogu97@gmail.com>
2020-09-11 12:28:44 +0300

    Refactored SkillViewModelFactory.java

--- app/src/main/java/com/joenjogu/gadsleaderboard/SkillViewModelFactory.java
+++ app/src/main/java/com/joenjogu/gadsleaderboard/SkillViewModelFactory.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+@SuppressWarnings("unchecked")
 public class SkillViewModelFactory extends ViewModelProvider.NewInstanceFactory {
 
     private NetworkRepository repository;
