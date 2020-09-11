d0f6da6b647414ed61debb6de8f948d3456d4e91
Joe Njogu <joenjogu97@gmail.com>
2020-09-11 12:08:10 +0300

    Refactored SkillFragment.java

--- app/src/main/java/com/joenjogu/gadsleaderboard/SkillFragment.java
+++ app/src/main/java/com/joenjogu/gadsleaderboard/SkillFragment.java
@@ -9,23 +9,26 @@ import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+import java.util.Objects;
 
+@SuppressWarnings("FieldCanBeLocal")
 public class SkillFragment extends Fragment {
 
     private NetworkRepository repository;
     private SkillViewModelFactory skillViewModelFactory;
     private SkillViewModel skillViewModel;
-    private RecyclerView recyclerView;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState
+    ) {
         return inflater.inflate(R.layout.fragment_skill, container, false);
     }
 
@@ -38,7 +41,9 @@ public class SkillFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        ProgressBar progressBar = getView().findViewById(R.id.skill_progress_bar);
+        ProgressBar progressBar = Objects.requireNonNull(
+                getView()).findViewById(R.id.skill_progress_bar
+        );
         RecyclerView recyclerView = getView().findViewById(R.id.iq_recyclerView);
         SkillAdapter adapter = new SkillAdapter(getContext());
         recyclerView.setAdapter(adapter);
@@ -46,7 +51,10 @@ public class SkillFragment extends Fragment {
 
         repository = new NetworkRepository();
         skillViewModelFactory = new SkillViewModelFactory(repository);
-        skillViewModel = new ViewModelProvider(this, skillViewModelFactory).get(SkillViewModel.class);
+        skillViewModel = new ViewModelProvider(
+                this,
+                skillViewModelFactory
+        ).get(SkillViewModel.class);
 
         progressBar.setVisibility(View.VISIBLE);
 
