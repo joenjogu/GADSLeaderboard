692a68da3c16b6264c845ccc9ece6067ce8f5341
Joe Njogu <joenjogu97@gmail.com>
2020-09-11 12:11:07 +0300

    Refactored FragmentAdapter.java

--- app/src/main/java/com/joenjogu/gadsleaderboard/FragmentAdapter.java
+++ app/src/main/java/com/joenjogu/gadsleaderboard/FragmentAdapter.java
@@ -2,14 +2,12 @@ package com.joenjogu.gadsleaderboard;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 
 public class FragmentAdapter extends FragmentStateAdapter {
-
 
     public FragmentAdapter(@NonNull FragmentManager fragmentManager, Lifecycle lifecycle) {
         super(fragmentManager, lifecycle);
